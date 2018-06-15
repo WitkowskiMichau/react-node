7a04d07fa7bfcedd3697acacb8c4135410f01e3a
Michal.Witkowski <batoponton@gmail.com>
2018-06-15 14:10:04 +0200

    App with google auth ready for payment

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{textAlign:'center'}}>
+      <h1>Emalily</h1>
+      <p>collect feedback from your user</p>
+    </div>
+  )
+}
+
+export default Landing;
